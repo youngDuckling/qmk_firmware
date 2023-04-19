44318e1acc0be178537e26f3291900aa88bdd0e3
Idan Kamara <i@idank.me>
2023-04-19 16:55:42 +0300

    crkbd: enable rgblight so via stops complaining about missing keycodes

--- keyboards/crkbd/keymaps/via/rules.mk
+++ keyboards/crkbd/keymaps/via/rules.mk
@@ -1,5 +1,5 @@
 MOUSEKEY_ENABLE     = yes     # Mouse keys
-RGBLIGHT_ENABLE     = no    # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
 VIA_ENABLE      = yes    # Enable VIA
 OLED_ENABLE     = no
 OLED_DRIVER     = SSD1306
