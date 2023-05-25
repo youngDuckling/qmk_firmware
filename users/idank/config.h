7fa2e9526ff897521db93455f6ef5cb318178b33
Idan Kamara <i@idank.me>
2023-05-25 13:27:09 +0300

    move things to config.h so they are picked up properly

---
+++ users/idank/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// We default to the right hand being the master.
+#undef MASTER_LEFT
+#undef EE_HANDS
+#define MASTER_RIGHT
+
+// Index finger trackball needs to be rotated 90 degrees.
+#ifndef TRACKBALL_THUMB
+#define POINTING_DEVICE_ROTATION_90
+#endif
--- users/idank/idank.h
+++ users/idank/idank.h
@@ -1,13 +1,3 @@
 #pragma once
 
 #include QMK_KEYBOARD_H
-
-// We default to the right hand being the master.
-#undef MASTER_LEFT
-#undef EE_HANDS
-#define MASTER_RIGHT
-
-// Index finger trackball needs to be rotated 90 degrees.
-#ifndef TRACKBALL_THUMB
-#define POINTING_DEVICE_ROTATION_90
-#endif
