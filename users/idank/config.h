1db6a20cb66a7e2fab0d084a5653f4646dbfe586
Idan Kamara <i@idank.me>
2023-08-20 22:57:20 +0300

    users/idank: rename TRACKBALL_POSITION to POINTING_DEVICE_POSITION

--- users/idank/config.h
+++ users/idank/config.h
@@ -1,24 +1,23 @@
 #pragma once
 
-// We default to the right hand being the master.
 #undef MASTER_LEFT
 #undef EE_HANDS
 #undef MASTER_RIGHT
 
-#ifdef TRACKBALL_POSITION_LEFT
+#ifdef POINTING_DEVICE_POSITION_LEFT
 #define MASTER_LEFT
 #else
 #define MASTER_RIGHT
 #endif
 
 
-#ifdef TRACKBALL_POSITION_LEFT
+#ifdef POINTING_DEVICE_POSITION_LEFT
 #define POINTING_DEVICE_ROTATION_270
 #endif
-#ifdef TRACKBALL_POSITION_RIGHT
+#ifdef POINTING_DEVICE_POSITION_RIGHT
 #define POINTING_DEVICE_ROTATION_90
 #endif
-#if defined(TRACKBALL_POSITION_THUMB) || defined(TRACKBALL_POSITION_MIDDLE)
+#if defined(POINTING_DEVICE_POSITION_THUMB) || defined(POINTING_DEVICE_POSITION_MIDDLE)
 #endif
 
 // Common settings to both busywait and interrupt.
@@ -28,14 +27,11 @@
 
 #define PS2_CLOCK_PIN   D3
 #define PS2_DATA_PIN    B4
-#endif
 
-#ifdef PS2_DRIVER_BUSYWAIT
 #define PS2_MOUSE_USE_REMOTE_MODE
 #endif
 
 #ifdef PS2_DRIVER_INTERRUPT
-#define PS2_MOUSE_USE_REMOTE_MODE
 #define PS2_INT_INIT()  do {    \
     EICRA |= ((1<<ISC31) |      \
               (0<<ISC30));      \
--- users/idank/rules.mk
+++ users/idank/rules.mk
@@ -4,22 +4,22 @@
 RGBLIGHT_SUPPORTED = no
 RGB_MATRIX_SUPPORTED = no
 
+ifeq ($(strip $(POINTING_DEVICE_POSITION)), left)
+	OPT_DEFS += -DPOINTING_DEVICE_POSITION_LEFT
+else ifeq ($(strip $(POINTING_DEVICE_POSITION)), thumb)
+	OPT_DEFS += -DPOINTING_DEVICE_POSITION_THUMB
+else ifeq ($(strip $(POINTING_DEVICE_POSITION)), middle)
+	OPT_DEFS += -DPOINTING_DEVICE_POSITION_MIDDLE
+else
+	OPT_DEFS += -DPOINTING_DEVICE_POSITION_RIGHT
+endif
+
 ifeq ($(strip $(POINTING_DEVICE)), trackball)
 	POINTING_DEVICE_ENABLE = yes
 	POINTING_DEVICE_DRIVER = pimoroni_trackball
 
 	ifeq ($(strip $(TRACKBALL_RGB_RAINBOW)), yes)
 		SRC += quantum/color.c $(USER_PATH)/trackball_rgb_rainbow.c
-	endif
-
-	ifeq ($(strip $(TRACKBALL_POSITION)), left)
-		OPT_DEFS += -DTRACKBALL_POSITION_LEFT
-	else ifeq ($(strip $(TRACKBALL_POSITION)), thumb)
-		OPT_DEFS += -DTRACKBALL_POSITION_THUMB
-	else ifeq ($(strip $(TRACKBALL_POSITION)), middle)
-		OPT_DEFS += -DTRACKBALL_POSITION_MIDDLE
-	else
-		OPT_DEFS += -DTRACKBALL_POSITION_RIGHT
 	endif
 endif
 
