0c8c05aee92f927a178bb2d5618e0e61d221e5d2
Idan Kamara <i@idank.me>
2023-09-22 11:17:10 +0300

    test ps2 vendor

--- keyboards/idank/sweeq/config.h
+++ keyboards/idank/sweeq/config.h
@@ -4,3 +4,8 @@
 #pragma once
 
 #define MASTER_RIGHT
+#define RP2040_FLASH_GD25Q64CS
+#define PS2_PIO_USE_PIO1
+#define PS2_MOUSE_INIT_DELAY 1000
+#define PS2_MOUSE_INVERT_X
+#define PS2_MOUSE_INVERT_Y
