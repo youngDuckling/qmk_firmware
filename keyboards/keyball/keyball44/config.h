80412d383e6383f873c14b6f2f1ac93cb798de2f
Idan Kamara <i@idank.me>
2024-02-15 21:39:35 +0200

    fix keyball configs

--- keyboards/keyball/keyball44/config.h
+++ keyboards/keyball/keyball44/config.h
@@ -21,19 +21,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Key matrix parameters
 #define MATRIX_ROWS         (4 * 2)  // split keyboard
 #define MATRIX_COLS         6
-//#define MATRIX_ROW_PINS     { F4, F5, F6, F7 }
 #define MATRIX_ROW_PINS     { GP29, GP28, GP27, GP26 }
-//#define MATRIX_COL_PINS     { D4, C6, D7, E6, B4, B5 }
 #define MATRIX_COL_PINS     { GP4, GP5, GP6, GP7, GP8, GP9 }
 #define MATRIX_MASKED
 #define DEBOUNCE            5
 #define DIODE_DIRECTION     COL2ROW
 
 // Split parameters
-//#define SOFT_SERIAL_PIN         D2
-//#define SPLIT_HAND_MATRIX_GRID  F7, D4
 #define SERIAL_USART_TX_PIN GP1
 #define SPLIT_HAND_MATRIX_GRID  GP26, GP4
+#define SPLIT_HAND_MATRIX_GRID_LOW_IS_LEFT
 //#define SPLIT_USB_DETECT
 //#define SPLIT_USB_TIMEOUT       500
 
@@ -42,7 +39,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // RGB LED settings
 #define WS2812_DI_PIN       GP0
 #ifdef RGBLIGHT_ENABLE
-#    define RGBLED_NUM      60
+#    define RGBLIGHT_LED_COUNT      60
 #    define RGBLED_SPLIT    { 30, 30 }  // (30 + 29)
 #    ifndef RGBLIGHT_LIMIT_VAL
 #        define RGBLIGHT_LIMIT_VAL  150 // limitated for power consumption
