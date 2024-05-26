40b3242c55f5cab105a97fa2529ca0fa6a6c21ec
Idan Kamara <i@idank.me>
2024-05-26 20:37:06 +0300

    keyball61: bump rgblight limit

--- keyboards/keyball/keyball61/config.h
+++ keyboards/keyball/keyball61/config.h
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGBLIGHT_LED_COUNT      74
 #    define RGBLED_SPLIT    { 37, 37 }
 #    ifndef RGBLIGHT_LIMIT_VAL
-#        define RGBLIGHT_LIMIT_VAL  30 // limitated for power consumption
+#        define RGBLIGHT_LIMIT_VAL  80 // limitated for power consumption
 #    endif
 #    ifndef RGBLIGHT_VAL_STEP
 #        define RGBLIGHT_VAL_STEP   12
