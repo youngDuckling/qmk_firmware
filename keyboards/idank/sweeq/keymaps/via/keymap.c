090e3ed360a4bebdc26a8236afd08c634bae58b9
youngDuckling <vbfrasson@gmail.com>
2024-08-21 18:56:20 +0200

    testing: changed E to R

--- keyboards/idank/sweeq/keymaps/via/keymap.c
+++ keyboards/idank/sweeq/keymaps/via/keymap.c
@@ -14,7 +14,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
+    KC_Q,    KC_W,    KC_R,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
     KC_CTLA, KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN,
     KC_LSHZ, KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, KC_RLSH,
                                     KC_CLGV, KC_BSM1, KC_SPM2, KC_GUTA
